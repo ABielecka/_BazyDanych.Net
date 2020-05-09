70e579f2641bd887aefc011f091543d377f00b26
Anna Bielecka <blank.anna42@gmail.com>
2020-05-10 01:56:15 +0200

    Prepared folder/space for Unit Tests.

---
+++ DatabaseW.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DatabaseW.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DatabaseW.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4584d077-7073-47ed-8de2-c4af3624861b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DatabaseW.Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using DatabaseW;
+
+namespace DatabaseW.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        //[TestMethod]
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
