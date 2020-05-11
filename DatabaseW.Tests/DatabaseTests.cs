2588d15bdd1c6f71d7b22358dc033e8a38000455
AlutkaG <alagerlach@interia.pl>
2020-05-11 11:40:08 +0200

    Change project name from 'DatabaseW.Tests' to 'DatabaseWTests' and 'Tests.cs' to 'DatabaseTests.cs'

--- DatabaseW.Tests/DatabaseTests.cs
+++ DatabaseW.Tests/DatabaseTests.cs
@@ -2,22 +2,17 @@
 using DatabaseW;
 using DatabaseW.Views.APIs;
 
-namespace DatabaseW.Tests
+namespace DatabaseWTests
 {
-    public class DatabaseWTests
+    [TestFixture]
+    public class DatabaseTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void DistanceAndDurationTest()
         {
             string url = "https://maps.googleapis.com/maps/api/distancematrix/xml?origins=AleksandraFredry2%20Gliwice%20Slaskie%20Polska&destinations=ul.Zwyci%C4%99stwa49%20CreditAgricoleBankPolskaS.A.%2044-100Gliwice%20Poland&mode=driving&sensor=false&language=en-EN&units=imperial&key=AIzaSyDxAC7sQJdA9a9LUIjmqf13oEY-whT8CEI&fbclid=IwAR1LV9L_5cB6e_4UmO6Go-Y5kkpl6VivhJVbsBCrpwREquLrbCt_DThcAvg";
-            PageAPIDetail distMatrix = new PageAPIDetail("Aleksandra Fredry 20, Gliwice Śląskie, Polska");
             vmDistance pom;
-            pom = distMatrix.DistanceAndDuration(url);
+            pom = PageAPIDetail.DistanceAndDuration(url);
             Assert.GreaterOrEqual(pom.distance, 0, "Error! Distance is less than 0!");
         }
 
